0791c72f1da7264a6605173f6bdae241a4d796e4
efrainmejiasc <efrainmejiasc@hotmail.com>
2020-06-20 16:03:01 -0400

    ListaCarpeta

--- MarketingDigital/MarketingDigitalDesktop/Forms/ListaCarpeta.Designer.cs
+++ MarketingDigital/MarketingDigitalDesktop/Forms/ListaCarpeta.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(533, 31);
             this.button1.TabIndex = 4;
-            this.button1.Text = "CARPETAS";
+            this.button1.Text = "DETALLE CARPETA";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
