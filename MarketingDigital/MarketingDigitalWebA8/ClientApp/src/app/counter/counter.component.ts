f574ca4a1b7f533cbcd85d1fe8c94b111fc9313c
efrainmejiasc <efrainmejiasc@hotmail.com>
2020-07-05 19:38:44 -0400

    Download

--- MarketingDigital/MarketingDigitalWebA8/ClientApp/src/app/counter/counter.component.ts
+++ MarketingDigital/MarketingDigitalWebA8/ClientApp/src/app/counter/counter.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html',
  styleUrls: ['./counter.component.css']
 })
-export class CounterComponent {
+
+export class CounterComponent implements OnInit {
   miImagen = "assets/image/Efrain.png";
-  public currentCount = 0;
+  public currentCount = 0
+
+
+  ngOnInit() {
+    alert("Hola Mundo");  
+  }
 
   public incrementCounter() {
     this.currentCount++;
   }
+
+
 }
